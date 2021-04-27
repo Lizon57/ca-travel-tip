962309c032a11f522bcb88196ab063fbc70c00c7
O Y <orenyaniv90@gmail.com>
2021-04-27 15:10:34 +0300

    api change

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -40,7 +40,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = ''; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyCE1_WVPkA6w51_K5aGvvNmCUb-agGUJi4';
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
