e30caa0716976e211ccf2458826bef0f8240e466
itay rosental <briskeritay3@gmail.com>
2021-04-27 15:35:28 +0300

    committt

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -8,24 +8,19 @@ var gMap;
 
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
+    console.log('InitMap');
     return _connectGoogleApi()
         .then(() => {
+            console.log('google available');
             gMap = new google.maps.Map(
                 document.querySelector('#map'), {
-                center: { lat, lng },
-                zoom: 15
-            })
-            gMap.addListener("click", (mapsMouseEvent) => {
-                // get clicked position 
-                const currPos = mapsMouseEvent.latLng.toJSON();
-                // pass clicked position to add marker
-                addMarker(currPos);
-            })
+                    center: { lat, lng },
+                    zoom: 15
+                })
+            console.log('Map!', gMap);
         })
 
 }
-
-
 
 function addMarker(loc) {
     var marker = new google.maps.Marker({
